38833cd633ce46cb2ae276d2d6bea2a3ea9d2ed6
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2021-09-24 10:37:43 +0200

    initial state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,47 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject, zip } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+type Durum = ['flat bread', 'meat', 'souse', 'tomato', 'cabbage'];
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+    <button (click)="_flatBread.next('flat bread')">Add Flat Bread</button>
+    <button (click)="_meat.next('meat')">Add Meat</button>
+    <button (click)="_souse.next('souse')">Add Souse</button>
+    <button (click)="_tomato.next('tomato')">Add Tomato</button>
+    <button (click)="_cabbage.next('cabbage')">Add Cabbage</button>
+    <ng-container *ngIf="durum$ | async as durum">
+      <section *ngIf="durum?.length > 0">
+        <h4>Enjoy</h4>
+        <img src="assets/durum.jpeg" alt="" width="400">
+
+        <h5>Ingredients:</h5>
+        <pre>{{ durum | json }}</pre>
+      </section>
+    </ng-container>
   `,
   styles: []
 })
-export class AppComponent {
-  title = 'angular-rxjs-demos';
+export class AppComponent implements OnInit {
+  durum$: Observable<Durum>;
+
+  _flatBread = new Subject<'flat bread'>();
+  _meat = new Subject<'meat'>();
+  _souse = new Subject<'souse'>();
+  _tomato = new Subject<'tomato'>();
+  _cabbage = new Subject<'cabbage'>();
+
+  ngOnInit(): void {
+    this.durum$ = zip(
+      this._flatBread,
+      this._meat,
+      this._souse,
+      this._tomato,
+      this._cabbage
+    ).pipe(
+      tap((durum) => console.log('Enjoy!', durum))
+    );
+  }
 }
